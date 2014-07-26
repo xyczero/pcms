db83578ba81e6ef41579c1c3f7c3dfd307842c15
xyczero <xyczero@sina.com>
2014-07-27 02:10:40 +0800

    optimize the url mapping and add about.html

--- blog/urls.py
+++ blog/urls.py
@@ -8,7 +8,15 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
                        
-    url(r'^$', views.index, name="index"),
-    url(r'^article/$', views.article, name="article"),
+    url(r'^$', views.index, {'ct_name': 'home'},name='index'),
+    
+    url(r'^category/(?P<ct_name>\w+)/', views.index, name='category'),
+    
+    url(r'^tag/(?P<ct_name>\w+)/', views.index, name='tag'),
+    
+    url(r'^article/(?P<article_id>\d+)/$', views.article, name='article'),
+    
     url(r'^archive/(?P<published_year>\d{4})/(?P<published_month>\d{1,2})/$', views.archive, name="archive"),
+    
+    url(r'^about/$', views.about, name='about'),
 )
--- blog/views.py
+++ blog/views.py
@@ -1,39 +1,33 @@
 # -*-coding:utf-8 -*-
-from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage, \
+    InvalidPage
 from django.shortcuts import render
 
 from blog.models import Blog, Tag, Category
 
 
 # Create your views here.
-def index(request):
-    if request.GET.get('tag'):
-        page_num = 1000
-        is_category = False;
-        is_tag = Tag.objects.get(id=request.GET.get('tag'))
-        blog_list = Blog.objects.filter(tags__id=request.GET.get('tag'), is_release=True).order_by('-create_time')
-    elif request.GET.get('category'):
-        page_num = 1000
-        is_tag = False;
-        is_category = Category.objects.get(id=request.GET.get('category'))
-        blog_list = Blog.objects.filter(category__id=request.GET.get('category'), is_release=True).order_by('-create_time')
-    else:
+def index(request, ct_name):
+    if ct_name.lower() == 'home':
         page_num = 5
-        is_tag = False;
-        is_category = False;
+        is_ct = False
         blog_list = Blog.objects.filter(is_release=True).order_by('-create_time')
-    
+    else:
+        page_num = 1000
+        is_ct = True;
+        blog_list = Blog.objects.filter(category__name=ct_name, is_release=True).order_by('-create_time')
+        if not blog_list:
+            blog_list = Blog.objects.filter(tags__name=ct_name, is_release=True).order_by('-create_time')
+            
     blogs = blog_paginator(request, blog_list, page_num)    
-    categories,new_blogs, date_archive_list = get_aside_init()
-    return render(request, 'blog/blog_list.html', {'blogs':blogs, 'new_blogs':new_blogs, 'categories':categories,'tags':Tag.objects.all(), 'date_archive_list':date_archive_list, 'is_tag':is_tag, 'is_category':is_category, })
+    categories, new_blogs, date_archive_list = get_aside_init()
+    return render(request, 'blog/blog_list.html', {'blogs':blogs, 'new_blogs':new_blogs, 'categories':categories, 'tags':Tag.objects.all(), 'date_archive_list':date_archive_list, 'is_ct':is_ct, }) 
 
-
-def article(request):
-    if request.GET.get('blog'):
-        blog = Blog.objects.get(id=request.GET.get('blog'), is_release=True)
+def article(request, article_id):
+    blog = Blog.objects.get(id=int(article_id), is_release=True)
         
-    categories,new_blogs, date_archive_list = get_aside_init()
-    return render(request, 'blog/blog_article.html', {'blog':blog, 'new_blogs':new_blogs, 'categories':categories,'tags':Tag.objects.all(), 'date_archive_list':date_archive_list, })
+    categories, new_blogs, date_archive_list = get_aside_init()
+    return render(request, 'blog/blog_article.html', {'blog':blog, 'new_blogs':new_blogs, 'categories':categories, 'tags':Tag.objects.all(), 'date_archive_list':date_archive_list, })
 
 
 def archive(request, published_year, published_month):
@@ -42,9 +36,12 @@ def archive(request, published_year, published_month):
     for blog in Blog.objects.all():
         if  str(blog.create_time.year) == published_year and  str(blog.create_time.month) == published_month and blog.is_release == True:
             blogs.append(blog)
-    categories,new_blogs, date_archive_list = get_aside_init()
-    return render(request, 'blog/blog_archive.html', {'blogs':blogs, 'new_blogs':new_blogs,'categories':categories, 'tags':Tag.objects.all(), 'date_archive_list':date_archive_list, 'published_year':published_year, 'published_month':published_month, })
+    categories, new_blogs, date_archive_list = get_aside_init()
+    return render(request, 'blog/blog_archive.html', {'blogs':blogs, 'new_blogs':new_blogs, 'categories':categories, 'tags':Tag.objects.all(), 'date_archive_list':date_archive_list, 'published_year':published_year, 'published_month':published_month, })
 
+def about(request):
+    categories, new_blogs, date_archive_list = get_aside_init()
+    return render(request, 'blog/about.html', {'new_blogs':new_blogs, 'categories':categories, 'tags':Tag.objects.all(), 'date_archive_list':date_archive_list, })
 
 def blog_paginator(request, blog_list, page_num):
     paginator = Paginator(blog_list, page_num)
@@ -58,9 +55,9 @@ def blog_paginator(request, blog_list, page_num):
     return blogs
 
 def get_aside_init():
-    categories=Category.objects.all()
+    categories = Category.objects.all()
     blogs = Blog.objects.filter(is_release=True).order_by('-create_time');
-    return categories,blogs[0:5], get_archive_date(blogs)
+    return categories, blogs[0:5], get_archive_date(blogs)
 
 def get_archive_date(blogs):
     years = []
