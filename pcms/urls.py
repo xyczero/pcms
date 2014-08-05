3329450dc12a3ae2ca6c5b67d6a533cc3af47fa9
root <root@AY140719162803905a1aZ.(none)>
2014-08-05 23:14:20 +0800

    change some details to released

--- pcms/urls.py
+++ pcms/urls.py
@@ -4,7 +4,8 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-                       
+    
+    url(r'^$', include('blog.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^blog/', include('blog.urls', namespace='blog')),
 )
